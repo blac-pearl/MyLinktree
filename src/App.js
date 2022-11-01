5deff5bfa2f64d99eec712d062bb834fa940db81
Chituru Precious <chiturupreciouspearl@gmail.com>
2022-11-01 17:28:42 +0100

    First commit of Linktree

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Linktree from "./Linktree"
+
+function App() {
+  return (
+    <div className="App">
+      <Linktree />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Linktree.js
@@ -0,0 +1,14 @@
+import Footer from "./Footer"
+import Links from "./Links"
+import Profile from "./Profile"
+
+const Linktree = () => {
+    return (
+        <>
+        <Profile />
+        <Links />
+        <Footer />
+        </>
+    );
+}
+export default Linktree;
