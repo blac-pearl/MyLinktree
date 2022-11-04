ffb66c27b947da26af636d3d7c809c07e29d4062
Chituru Precious <chiturupreciouspearl@gmail.com>
2022-11-04 20:40:09 +0100

    Feat : Added react router dom.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import Linktree from "./Linktree";
+import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
 import Contact from "./Contact"
 
-function App() {
+const App = () => {
   return (
+    <Router>
     <div className="App">
-      <Linktree />
+      <Routes>
+      <Route path="/" element={<Linktree />} />
+      <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
+    </Router>
   );
 }
 
